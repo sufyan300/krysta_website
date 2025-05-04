89c5ebdf49653a1edc4251524c06ef010a3febe1
sufyan300 <sufyanlogan786@gmail.com>
2025-05-04 23:31:46 +0530

    Updates

---
+++ _next/static/chunks/app/page-b6bf788c2b1d6d1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{333:()=>{},1032:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,333,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[916,63,441,684,358],()=>s(1032)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/product/page-1a3f8adcf451cabb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[362,402,478,493,701,805,910],{5616:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,441,684,358],()=>s(5616)),_N_E=e.O()}]);
